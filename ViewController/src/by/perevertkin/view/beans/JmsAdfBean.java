08d874948d8cf5b6602dc54da57285e387a00a3e
Perevertkin Mikhail <perevertkinma@gmail.com>
2016-02-13 21:05:07 +0300

    version 2 (many Messages)

--- ViewController/src/by/perevertkin/view/beans/JmsAdfBean.java
+++ ViewController/src/by/perevertkin/view/beans/JmsAdfBean.java
@@ -11,6 +11,8 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import oracle.adf.share.logging.ADFLogger;
+
 public class JmsAdfBean {
     private RichOutputText recieveMessageText;
     private RichInputText sendMessageText;
@@ -27,40 +29,92 @@ public class JmsAdfBean {
     private Queue queue;
     private TextMessage msg;
     private Context initialContext;
+    private RichInputText repeatCount;
+    
+    private static ADFLogger logger = 
+                ADFLogger.createADFLogger(JmsAdfBean.class); 
 
-    public JmsAdfBean() throws NamingException, JMSException {
+    /*  
+     * initial JMS
+     * */    
+
+    public JmsAdfBean() {
+        try{
         initialContext = new InitialContext();
         qconFactory = (QueueConnectionFactory) initialContext.lookup(JMS_FACTORY);
         queue = (Queue) initialContext.lookup(QUEUE);
+
+        } catch (NamingException e) {
+            logger.severe("NamingException in constructor():"+e.getMessage());
+        }
     }
 
+
+    /*
+     * Close session and connection
+     * */
+    
     public void close() throws JMSException {
         qsession.close();
         qcon.close();
     }
-
-    public void sendMessage(ActionEvent actionEvent) throws JMSException, NamingException {
+    
+    /*
+     * Send message to WebLogic JMS
+     * */    
+    
+    public void sendMessage(ActionEvent actionEvent) {
+        try {
         qcon = qconFactory.createQueueConnection();
         qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
         qsender = qsession.createSender(queue);
         msg = qsession.createTextMessage();
         qcon.start();
-        msg.setText(getSendMessageText().getValue().toString());
-        qsender.send(msg);
+        Integer count = Integer.valueOf(getRepeatCount().getValue().toString());
+        for (int i = 0; i < count; i++) {
+            msg.setText(getSendMessageText().getValue().toString());
+            qsender.send(msg);
+        }
         qsender.close();
         close();
+
+        } catch (JMSException e) {
+            logger.severe("JMSException in sendMessage():"+e.getMessage());
+        }
     }
 
-    public void readMessage(ActionEvent actionEvent) throws NamingException, JMSException {
-        qcon = qconFactory.createQueueConnection();
-        qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-        qreceiever = qsession.createReceiver(queue);
-        msg = qsession.createTextMessage();
-        qcon.start();
-        TextMessage textMessage = (TextMessage) qreceiever.receive();
-        recieveMessageText.setValue(textMessage.getText());
-        qreceiever.close();
-        close();
+    /*
+     * Read message from WebLogic JMS
+     * */    
+    public void readMessage(ActionEvent actionEvent) {
+        try {
+            String returnJms = "Msg:";
+            qcon = qconFactory.createQueueConnection();
+            qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+            qreceiever = qsession.createReceiver(queue);
+            msg = qsession.createTextMessage();
+            qcon.start();
+            Message m;
+            do {
+                m = qreceiever.receive(1);
+                if (m != null) {
+                    if (m instanceof TextMessage) {
+                        msg = (TextMessage) m;
+                        returnJms = returnJms + msg.getText() + " ";
+                    } else {
+                        break;
+                    }
+                }
+            } while (m != null);
+
+            recieveMessageText.setValue(returnJms);
+            qreceiever.close();
+            close();
+
+        } catch (JMSException e) {
+            logger.severe("JMSException in readMessage():"+e.getMessage());
+        }
+
     }
 
     public void setSendMessageText(RichInputText sendMessageText) {
@@ -70,11 +124,20 @@ public class JmsAdfBean {
     public RichInputText getSendMessageText() {
         return sendMessageText;
     }
+
     public void setRecieveMessageText(RichOutputText recieveMessageText) {
         this.recieveMessageText = recieveMessageText;
     }
 
     public RichOutputText getRecieveMessageText() {
         return recieveMessageText;
+    }
+
+    public void setRepeatCount(RichInputText repeatCount) {
+        this.repeatCount = repeatCount;
+    }
+
+    public RichInputText getRepeatCount() {
+        return repeatCount;
     }
 }
